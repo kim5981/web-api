84cfb18054c3fadfc849111adf7461d67a55669d
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-13 18:46:24 -0700

    get actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,5 +2,29 @@ const express = require("express")
 
 const router = express.Router()
 
+const {
+    validateActionId,
+} = require("./actions-middlware")
+
 const Actions = require("./actions-model")
+
+//* GET /api/actions
+router.get("/", (req, res, next) => {
+    Actions.get()
+    .then(actions => {
+        res.json(actions)
+    })
+    .catch(next)
+})
+
+//* GET /api/actions/:id
+
+//* POST /api/actions
+
+//* PUT /api/actions/:id
+
+//* DELETE /api/actions/:id
+
+
+
 module.exports = router
